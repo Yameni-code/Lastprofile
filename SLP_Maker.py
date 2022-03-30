217007572eebc6fb42517a47428ba7f13074c8e1
Yameni-code <yamenigleen@gmail.com>
2022-03-30 14:01:04 +0200

    import weekend xlsx files

--- SLP_Maker.py
+++ SLP_Maker.py
@@ -23,13 +23,14 @@ def change_filename(filepath, week):
     print("Changing filename Complet...")
     return new_filepath
 
-def excel_operation(path1, path2, path3, path4, path5):
+def excel_operation(path1, path2):
+    #def excel_operation(path1, path2, path3, path4, path5):
     wb1 = load_workbook(MOTHER_PATH + path1)
     ws1 = wb1.active
 
     wb2 = load_workbook(MOTHER_PATH + path2)
     ws2 = wb2.active
-
+    """
     wb3 = load_workbook(MOTHER_PATH + path3)
     ws3 = wb3.active
 
@@ -38,18 +39,19 @@ def excel_operation(path1, path2, path3, path4, path5):
 
     wb5 = load_workbook(MOTHER_PATH + path5)
     ws5 = wb5.active
-
+    """
     for index in range(6, 102):
         z = "C" + str(index)
-        ws1[z].value = ( float(ws1[z].value) + float(ws2[z].value) + float(ws3[z].value) + float(ws4[z].value) + float(ws5[z].value)   ) / 5
+        ws1[z].value = ( float(ws1[z].value) + float(ws2[z].value)  ) / 2
+        #ws1[z].value = ( float(ws1[z].value) + float(ws2[z].value) + float(ws3[z].value) + float(ws4[z].value) + float(ws5[z].value)   ) / 5
 
     wb1.save(MOTHER_PATH + path1)
     print("Saving new workbook Complet...")
 
 def main():
-    weeks = ["kw_35"]
-    #for i in range(5, 51):
-     #   weeks.append("kw_" + str(i))
+    weeks = []
+    for i in range(5, 51):
+        weeks.append("kw_" + str(i))
 
     for week in weeks:
         print("****************")
@@ -57,16 +59,19 @@ def main():
         filenames = get_filenames(week)
         filepaths = []
 
-        if (len(filenames) == 5 ):
+        #if (len(filenames) == 2 ):
+        if (len(filenames) == 2 ):
             filepaths.append( week + "/" + filenames[0])
             filepaths.append( week + "/" + filenames[1])
+            """
             filepaths.append( week + "/" + filenames[2])
             filepaths.append( week + "/" + filenames[3])
             filepaths.append( week + "/" + filenames[4])
-        
+            """
             filepaths[0] = change_filename(filepaths[0], week)
             #ExcelExport_20220329_105216.xlsx
-            excel_operation(filepaths[0] , filepaths[1] , filepaths[2] , filepaths[3] , filepaths[4] )
+            excel_operation(filepaths[0] , filepaths[1] )
+            #excel_operation(filepaths[0] , filepaths[1] , filepaths[2] , filepaths[3] , filepaths[4] )
         else:
             print("Number of files incorrect")
             print(filenames)
@@ -77,7 +82,7 @@ def main():
     
 
 
-#main()
+main()
 
 
 
