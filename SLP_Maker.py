ac816d2eb1c1d34d90e7f5fe954d1b71b7fc18a8
Yameni-code <yamenigleen@gmail.com>
2022-03-30 10:48:57 +0200

    update

--- SLP_Maker.py
+++ SLP_Maker.py
@@ -4,21 +4,19 @@ Standardlastprofile calculation
 """
 
 import os
-import glob
 from openpyxl import Workbook, load_workbook
 
 # Constants
 EXTENTION = ".xlsx"
 
 def get_filenames(folder):
-    xlsx_files = []
-    for file in glob.glob( folder +"*.xlsx"):
-        xlsx_files.append(file)
+    xlsx_files = os.listdir("//cifs02/RoamingData$/u2110370/Documents/GitHub/Lastprofile/" + folder)
+    print("Getting filenames Complet...")
     return(xlsx_files)
 
 
 def change_filename(filepath, week):
-    new_filepath = "Lastprofile_" + week + EXTENTION
+    new_filepath = week + "/" +"Lastprofile_" + week + EXTENTION
     os.rename(filepath, new_filepath)
     print("Changing filename Complet...")
     return new_filepath
@@ -43,22 +41,30 @@ def excel_operation(path1, path2, path3, path4, path5):
         z = "C" + str(index)
         ws1[z].value = ( float(ws1[z].value) + float(ws2[z].value) + float(ws3[z].value) + float(ws4[z].value) + float(ws5[z].value)   ) / 5
 
+    wb1.save()
+
 def main():
-    week = "kw_5"
+    week = "kw_6"
 
-    print(get_filenames(week + "/"))
+    filenames = get_filenames(week)
+    filepaths = []
 
-    names = [105216, 105229, 105242, 105249, 105257]
-
-    file_path1 = week + "/" + "ExcelExport_20220329_" + str(names[0]) + EXTENTION
-    file_path2 = week + "/" + "ExcelExport_20220329_" + str(names[1]) + EXTENTION
-    file_path3 = week + "/" + "ExcelExport_20220329_" + str(names[2]) + EXTENTION
-    file_path4 = week + "/" + "ExcelExport_20220329_" + str(names[3]) + EXTENTION
-    file_path5 = week + "/" + "ExcelExport_20220329_" + str(names[4]) + EXTENTION
-
-    #file_path1 = change_filename(file_path1, week)
-
-    #excel_operation(file_path1, file_path2, file_path3, file_path4, file_path5)
+    if (len(filenames) == 5 ):
+        filepaths.append( week + "/" + filenames[0])
+        filepaths.append( week + "/" + filenames[1])
+        filepaths.append( week + "/" + filenames[2])
+        filepaths.append( week + "/" + filenames[3])
+        filepaths.append( week + "/" + filenames[4])
+        
+        filepaths[0] = change_filename(filepaths[0], week)
+        print(filepaths)
+        #ExcelExport_20220329_105609.xlsx
+        #excel_operation(filepaths[0] , filepaths[1] , filepaths[2] , filepaths[3] , filepaths[4] )
+    else:
+        print("Number of files incorrect")
+        print(filenames)
+    
+    
 
     
 
